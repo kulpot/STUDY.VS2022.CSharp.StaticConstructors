4dca21b535d7a7b540f63f81c4f74ae13739da61
Kulpot <kulpotwahu@gmail.com>
2023-05-15 21:36:35 +0800

    Add project files.

---
+++ StaticConstructors/Form1.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Forms;
+
+namespace StaticConstructors
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            MyControl control = new MyControl();
+
+            MyControl control1 = new MyControl();
+        }
+    }
+}
---
+++ StaticConstructors/MyControl.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StaticConstructors
+{
+    class MyControl : Control
+    {
+        public static Padding DefaultMargin { get; private set; }
+
+        static MyControl()//static constructor
+        {
+            bool isWinXP = true;
+            DefaultMargin = new Padding(isWinXP ? 4 : 6);
+        }
+
+        public MyControl()//instance constructor
+        {
+            Margin = DefaultMargin;
+        }
+    }
+}
